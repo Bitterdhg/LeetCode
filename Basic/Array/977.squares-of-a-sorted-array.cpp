b54c888fa14e67396682a245247904356bbf9deb
Bitterdhg <86940141+Bitterdhg@users.noreply.github.com>
2022-01-28 00:04:46 +0800

    1.27+

--- Basic/Array/977.squares-of-a-sorted-array.cpp
+++ Basic/Array/977.squares-of-a-sorted-array.cpp
@@ -9,30 +9,120 @@
 # include <iostream>
 # include <vector>
 using namespace std;
-
-int main(){
-    int nums[5] =  {-4,-1,0,3,10} ; 
-// Approach : two-poniter 时间复杂度（O（n））
-    //def 
-    int length = 5; 
-    int result[5];
+// 方法一：双指针法（从外到内）
+// int main(){
+//     int nums[5] =  {-4,-1,0,3,10} ; 
+// // Approach : two-poniter 时间复杂度（O（n））
+//     //def 
+//     int length = 5; 
+//     int result[5];
     
-    int leftPoniter = 0;
-    int rightPointer = length - 1;
-    int resultIndex = length - 1;
-    //1. left right search
-    for( ; leftPoniter <= rightPointer ; resultIndex-- ){
-            if (nums[leftPoniter] * nums[leftPoniter] > nums[rightPointer] * nums[rightPointer]){
-                result[resultIndex] = nums[leftPoniter] * nums[leftPoniter];
-                leftPoniter ++;
-            }else{
-                result[resultIndex] = nums[rightPointer] * nums[rightPointer];
-                rightPointer --;
-            }// if (left > right)
+//     int leftPoniter = 0;
+//     int rightPointer = length - 1;
+//     int resultIndex = length - 1;
+//     //1. left right search
+//     for( ; leftPoniter <= rightPointer ; resultIndex-- ){
+//             if (nums[leftPoniter] * nums[leftPoniter] > nums[rightPointer] * nums[rightPointer]){
+//                 result[resultIndex] = nums[leftPoniter] * nums[leftPoniter];
+//                 leftPoniter ++;
+//             }else{
+//                 result[resultIndex] = nums[rightPointer] * nums[rightPointer];
+//                 rightPointer --;
+//             }// if (left > right)
+//     }
+//     // 3. 最后 输出新的数组
+//     for(int i = 0 ; i <= length-1; i++){
+//         cout << result[i] << " ";
+//     }
+//     return 0;
+// }
+
+// 方法二：直接暴力（超时）
+// int main() {
+//     // 按照绝对值 重新排列
+//     vector <int> nums {-4,-1,0,3,10} ; 
+//     // def
+//     int length = nums.size() , right_bound = nums.size() - 1;
+
+//     // 1. square
+//     for (int i = 0 ; i <= length-1 ;i++ ){
+//         nums[i] = nums[i] * nums[i];
+//     }
+//     // 2. sort
+//     for (bool sort = false ; (sort = !sort) ; right_bound--){
+//         for (int i = 0; i <= right_bound-1 ; i++){
+//             if (nums[i] > nums[i+1]){
+//                 swap(nums[i] , nums[i+1]);
+//                 sort = false;
+//             }
+//         }
+//     }
+//     for (int i = 0; i <= length - 1; i++) {
+//         cout << nums[i] << " "; 
+//     }
+    
+// }
+
+// 法三：自己想的双指针（从中间往两边找）
+// 时间复杂度 O（n）
+
+int main() {
+    vector <int> nums {-4,-1,0,3,10} ; 
+    // def
+    int length = nums.size();
+    int min = 1e8 + 1;
+    int minI = -1;
+
+    int leftI = -1;
+    int rightI = -1;
+    int Result_i = -1;
+    vector<int> Result(length, 0);
+    // 1. 寻找最小值 ， 然后标记它的位置
+    for (int i = 0; i <= length-1 ; i++){
+        // 1.1 square
+        nums[i] = nums[i] * nums[i];
+        // 1.2 find MIN
+        if (min > nums[i]){
+            min = nums[i];
+            minI = i;
+        }
+    }
+
+    // 2. 分别 左右search 小的放入新的数组里
+    // 2.1 先到结尾
+    leftI = minI - 1;
+    rightI = minI + 1;
+    Result[0] = min;
+        for (Result_i = 1; (rightI <= length-1) && (leftI >= 0); ){ //Result_i 记录当前Result的index
+        if(nums[leftI] <= nums[rightI]){
+            Result[Result_i] = nums[leftI];
+            Result_i++;
+            leftI --;
+        }
+        else{
+            Result[Result_i] = nums[rightI];
+            Result_i++;
+            rightI++;
+        }
+    }
+    // 2.2 后到结尾
+    if (rightI <= length-1){ // 2.2.1 Right没到头
+        for ( ; Result_i<= length-1 ;){
+            Result[Result_i] = nums[rightI];
+            Result_i++;
+            rightI++;
+        }
+    }
+    else{ // // 2.2.2 Left没到头
+        for( ; Result_i<= length-1 ;){
+            Result[Result_i] = nums[leftI];
+            Result_i++;
+            leftI --;
+        } 
     }
     // 3. 最后 输出新的数组
-    for(int i = 0 ; i <= length-1; i++){
-        cout << result[i] << " ";
+    for (int i = 0; i <= length - 1; i++) {
+        cout << Result[i] << " "; 
     }
     return 0;
 }
--- Basic/String/848.shifting-letters.cpp
+++ Basic/String/848.shifting-letters.cpp
@@ -1,41 +1,63 @@
-// 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+// 有一个由小写字母组成的字符串 S，和一个整数数组 shifts。
 
-// 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+// 我们将字母表中的下一个字母称为原字母的 移位（由于字母表是环绕的， 'z'?将会变成?'a'）
 
-// 请你计算并返回达到楼梯顶部的最低花费。
 # include <iostream>
 # include <vector>
 # include <algorithm>
 using namespace std;
+// 自己写的题解
+// int main() {
+//         // 分析： 第0个字母右移动次数（后迷案的数字所有加起来 % 26）
+//         // 即：第 i 个字母右移动次数是 （i的后缀和 % 26 ）
+//     vector <int> shifts {3,5,9};
+//     string s = "abc";
+//     // def
+//     int length = shifts.size();
+//     int upbd = length - 1;
+//     int strlen = s.length();
+//     int i;
+//     int sign = 0;
+//     int times = 0;
+//     vector <long> sum(length + 1);
+    
+//     sum[upbd] = shifts[upbd];
+//     // 1. 后缀和
+//     for (i = upbd - 1; i >= 0; i--) { // sum[i] 指第i即i后面的数字的后缀和
+//         sum[i] = sum[i + 1] + shifts[i];
+//     } // sum[] = 17 14 9 
+//     // for (i = 0 ; i <= upbd ; i++)
+//     // cout << sum[i] << " ";
+    
+//     // 2. 遍历s的所有字母 并移动
+//     // cout << s.length();
+//     for (i = 0; i <= strlen - 1; i++) {
+//         sign = s[i] - 'a';
+//         times = (sum[i] + sign) % 26;
+        
+//         s[i] = times + 'a';
+//     }
+//     cout << s;
+// }
+
+// 参考别人的思路：从后往前扫
+// 跟我的本质一样，区别是 我将之储存了，而他没有。 我的空间复杂度会变高
 int main() {
-        // 分析： 第0个字母右移动次数（后迷案的数字所有加起来 % 26）
-        // 即：第 i 个字母右移动次数是 （i的后缀和 % 26 ）
     vector <int> shifts {3,5,9};
     string s = "abc";
+
     // def
+    int i;
     int length = shifts.size();
     int upbd = length - 1;
-    int strlen = s.length();
-    int i;
-    int sign = 0;
+    long sum = 0;
     int times = 0;
-    vector <long> sum(length + 1);
-    
-    sum[upbd] = shifts[upbd];
-    // 1. 后缀和
-    for (i = upbd - 1; i >= 0; i--) { // sum[i] 指第i即i后面的数字的后缀和
-        sum[i] = sum[i + 1] + shifts[i];
-    } // sum[] = 17 14 9 
-    // for (i = 0 ; i <= upbd ; i++)
-    // cout << sum[i] << " ";
-    
-    // 2. 遍历s的所有字母 并移动
-    // cout << s.length();
-    for (i = 0; i <= strlen - 1; i++) {
-        sign = s[i] - 'a';
-        times = (sum[i] + sign) % 26;
-        
-        s[i] = times + 'a';
+    // 1. 从后往前
+    for (i = upbd; i >= 0; i--) {
+        // 1.1 后缀和
+        sum += shifts[i];
+        s[i] = ((s[i] - 'a') + sum) % 26 + 'a';
     }
+    // 2.out 
     cout << s;
 }
