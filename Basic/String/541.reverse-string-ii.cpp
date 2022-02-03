528fc0640cfa2a841c658f6c65ba189fb9705b9f
Bitterdhg <86940141+Bitterdhg@users.noreply.github.com>
2022-02-03 21:11:11 +0800

    2.3

--- Basic/String/541.reverse-string-ii.cpp
+++ Basic/String/541.reverse-string-ii.cpp
@@ -42,41 +42,73 @@
 // }
 
 // 自解法二：双指针
+// # include <iostream>
+// # include <vector>
+// # include <algorithm>
+// using namespace std;
+// void reverse (string& s, int start, int end) { // 区间[]
+//     char temp;
+//     while (start <= end) {
+//         temp = s[start];
+//         s[start] = s[end];
+//         s[end] = temp; 
+
+//         start ++;
+//         end --;
+//     }
+// }
+
+// int main() {
+//     string s = "abcdefg";
+//     int k = 2;
+//     // def
+//     int length = s.length();
+//     int left = length; // 剩余的数量
+//     int upbd = length - 1;
+//     // reverse(s, 0, 3); // 引用传递
+//     // cout << s;
+//     while (left >= 1) {  
+//         if (left < k) { //剩余字符全部反转。
+//             reverse (s, upbd - left + 1,upbd); // 区间[]
+//             break;
+//         } else {
+//             reverse (s, upbd - left + 1,upbd - left + k); // 区间[]
+//             left -= 2 * k;
+//         } // if ((length - j) < k)
+//     }
+        
+//     cout << s;
+//     return 0;
+// }
+
+// 参考题解：模拟 + 库函数
 # include <iostream>
 # include <vector>
 # include <algorithm>
 using namespace std;
-void reverse (string& s, int start, int end) { // 区间[]
-    char temp;
-    while (start <= end) {
-        temp = s[start];
-        s[start] = s[end];
-        s[end] = temp; 
-
-        start ++;
-        end --;
-    }
-}
 
 int main() {
-    string s = "abcdefg";
+    string s = "abcd";
     int k = 2;
+
     // def
     int length = s.length();
-    int left = length; // 剩余的数量
     int upbd = length - 1;
-    // reverse(s, 0, 3); // 引用传递
-    // cout << s;
-    while (left >= 1) {  
-        if (left < k) { //剩余字符全部反转。
-            reverse (s, upbd - left + 1,upbd); // 区间[]
-            break;
-        } else {
-            reverse (s, upbd - left + 1,upbd - left + k); // 区间[]
-            left -= 2 * k;
-        } // if ((length - j) < k)
+    int i = 0; // 需要反转的首index
+    // reverse
+
+    // 少于 k 个 -> 剩余字符全部反转 ;
+    // 小于 2k 但大于或等于 k 个,则反转前 k 个字符，其余字符保持原样。
+    while ( i <= upbd ) { // 剩余数量 length - i
+        if (length - i < k) { // 少于 k 个  剩余字符全部反转
+            reverse(s.begin() + i, s.end());
+        } else { // 小于 2k 但大于或等于 k 个
+            reverse(s.begin() + i, s.begin() + i + k);// 区间:[) ;
+        } //if (length - i)
+        i += 2 * k;
     }
-        
+
+    // out 
     cout << s;
     return 0;
 }
--- Basic/String/JZOffer05.ti-huan-kong-ge-lcof.cpp
+++ Basic/String/JZOffer05.ti-huan-kong-ge-lcof.cpp
@@ -3,6 +3,36 @@
 // 输出："We%20are%20happy."
 
 // 自解一：生成新的string
+// 时间复杂度：O(n) 空间复杂度O（n）
+
+// # include <iostream>
+// # include <vector>
+// # include <algorithm>
+// using namespace std;
+// int main() {
+//     string s = "We are happy.";
+
+//     // def
+//     string str;
+//     int length = s.length();
+//     int i = 0;
+//     int j = -1; // 指当前new str的尾巴的index
+//     // find     
+//     while (i <= length - 1) {
+//         if (s[i] == ' ') {
+//             str += "%20";
+//             i ++;
+//         } else {
+//             str += s[i];
+//             i++;
+//         } // if (s[i] == ' ')
+//     }
+//     cout << str;
+//     return 0;
+// }
+
+
+// 看了题解后的思路：法二：双指针
 # include <iostream>
 # include <vector>
 # include <algorithm>
@@ -11,21 +41,27 @@ int main() {
     string s = "We are happy.";
 
     // def
-    string str;
     int length = s.length();
-    int i = 0;
-    int j = -1; // 指当前new str的尾巴的index
-    // find     
-    while (i <= length - 1) {
-        if (s[i] == ' ') {
-            str += "%20";
-            i ++;
-        } else {
-            str += s[i];
-            i++;
-        } // if (s[i] == ' ')
+    int leftPointer = length - 1; // 当前遍历的元素
+    int rightPointer = 3 * length - 1; // 当前被遍历的元素应该放的位置的第三个
+    int i;
+
+    // 倒着来
+    s.resize(3 * length);
+    for (; leftPointer >= 0; leftPointer--) {// 左指针到达Tail 结束
+         if (s[leftPointer] == ' ') {
+             s[rightPointer--] = '0';
+             s[rightPointer--] = '2';
+             s[rightPointer--] = '%';
+         } else {
+            s[rightPointer--] = s[leftPointer];
+         } // if (s[leftPointer] == ' ') 
     }
-    cout << str;
+
+    for (i = rightPointer + 1; i <= 3 * length - 1; i++) {
+        cout << s[i];
+    }
+    
     return 0;
 }
 
