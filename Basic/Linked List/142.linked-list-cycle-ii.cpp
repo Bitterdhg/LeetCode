26bb3494230a41aef3d9751fa29ce3d7b49fb55e
Bitterdhg <86940141+Bitterdhg@users.noreply.github.com>
2022-02-14 16:10:37 +0800

    链表总结

--- Basic/Linked List/142.linked-list-cycle-ii.cpp
+++ Basic/Linked List/142.linked-list-cycle-ii.cpp
@@ -6,6 +6,125 @@
 
 // 不允许修改 链表。
 
+// #include <iostream>
+// using namespace std;
+
+// //定义结构体
+// struct ListNode {
+// 	int val;
+// 	ListNode * next;
+// 	ListNode(int val) : val(val), next(NULL){}
+// };
+
+
+// class Solution
+// {
+// public:
+// 	/*创建单链表*/
+// 	void createList(ListNode *head)
+// 	{
+// 		int i;
+// 		ListNode* phead=head; //不破坏头指针
+// 		for(i=1;i<2;i++){
+// 			ListNode* node=new ListNode (i);
+// 			phead->next=node;
+// 			phead=node;
+// 		}
+//         ListNode* node=new ListNode (2);
+//         phead->next=node;
+// 		node->next=head;
+// 		cout<<"循环链表创建成功!\n";
+// 	}
+	
+// 	/*打印链表*/
+// 	void printList(ListNode* head)
+// 	{
+// 		ListNode* phead=head;
+// 		while(phead!=NULL)
+// 		{
+// 			cout<<phead->val<<" ";
+// 			phead=phead->next;
+// 		}
+// 		cout<<"\n";
+// 	}		
+// };
+
+// // 肯定有环
+// // du -> 0 -> 1 -> 2
+
+// int returnPos(ListNode *head) {
+//     // def
+//     ListNode * begin = head -> next; // 0
+//     ListNode * temp = head -> next; // 0
+//     ListNode * stdStemp = head -> next;
+//     int pos = 0;
+//     int num = 0;
+
+//     for (pos = 0; pos <= 10000 - 1; pos ++) {
+//         for (num = pos; num <= 10000 - 1; num ++) {
+//             // case 1 huan nei 
+//             if (temp -> next == begin) {
+//                 return pos;
+//             } else { // case 2 huan wai
+//                 temp = temp -> next;
+//                 continue;
+//             }// if (temp -> next == begin)
+//         }
+//         begin = begin -> next;
+//         stdStemp = stdStemp -> next;
+//         temp = stdStemp;
+//     }
+//     return -1;
+// }
+
+// // 法一：暴力
+// // 双指针比较next
+// // 1. len (判断是否有环)
+
+// int main()
+// {
+// 	ListNode* head=new ListNode (0);
+// 	Solution ll;
+
+// 	ll.createList(head);
+// 	// ---------------------
+//     // def
+//     int len = 0;
+//     int pos = -2;
+//     ListNode * dummyHead = new ListNode (-1);
+//     dummyHead -> next = head;
+
+//     // 1. yes or no circle
+//     while (head != NULL && len <= 10001) {
+//         len++;
+//         head = head -> next;
+//     }
+
+//     // 2. pos
+//     if (len != 10002) {
+//         pos = -1;
+//     } else {
+//         pos = returnPos(dummyHead);
+//     } //if (len != 10002)
+    
+//     // 3. out
+//     cout << pos;
+
+// 	// ---------------------
+// 	return 0;
+// }
+
+
+// 法二：双指针（很牛逼）
+
+// 给定一个链表的头节点 ?head?，返回链表开始入环的第一个节点。?如果链表无环，则返回?null。
+
+// 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 
+// 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+// 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+// 不允许修改 链表。
+
 #include <iostream>
 using namespace std;
 
@@ -20,7 +139,7 @@ struct ListNode {
 class Solution
 {
 public:
-	/*创建单链表*/
+	/*创建循环链表*/
 	void createList(ListNode *head)
 	{
 		int i;
@@ -49,37 +168,6 @@ public:
 	}		
 };
 
-// 肯定有环
-// du -> 0 -> 1 -> 2
-
-int returnPos(ListNode *head) {
-    // def
-    ListNode * begin = head -> next; // 0
-    ListNode * temp = head -> next; // 0
-    ListNode * stdStemp = head -> next;
-    int pos = 0;
-    int num = 0;
-
-    for (pos = 0; pos <= 10000 - 1; pos ++) {
-        for (num = pos; num <= 10000 - 1; num ++) {
-            // case 1 huan nei 
-            if (temp -> next == begin) {
-                return pos;
-            } else { // case 2 huan wai
-                temp = temp -> next;
-                continue;
-            }// if (temp -> next == begin)
-        }
-        begin = begin -> next;
-        stdStemp = stdStemp -> next;
-        temp = stdStemp;
-    }
-    return -1;
-}
-
-// 法一：暴力
-// 双指针比较next
-// 1. len (判断是否有环)
 
 int main()
 {
@@ -88,29 +176,46 @@ int main()
 
 	ll.createList(head);
 	// ---------------------
-    // def
-    int len = 0;
-    int pos = -2;
-    ListNode * dummyHead = new ListNode (-1);
-    dummyHead -> next = head;
+    // def 
+    ListNode * fastPointer = head;
+    ListNode * slowPointer = head;
+    ListNode * temp = head;
 
-    // 1. yes or no circle
-    while (head != NULL && len <= 10001) {
-        len++;
-        head = head -> next;
+    // 善用while循环 而不是if语句！！！！
+    while (fastPointer != NULL && fastPointer -> next != NULL) { // 重要！！！！！写入笔记
+        fastPointer = fastPointer -> next -> next; // 重要！！！！！写入笔记
+        slowPointer = slowPointer -> next; 
+        if (fastPointer == slowPointer) {
+            while (temp != slowPointer) {
+                temp = temp -> next;
+                slowPointer = slowPointer -> next;
+            }
+            ll.printList(temp);
+        }
     }
-
-    // 2. pos
-    if (len != 10002) {
-        pos = -1;
-    } else {
-        pos = returnPos(dummyHead);
-    } //if (len != 10002)
-    
-    // 3. out
-    cout << pos;
-
-	// ---------------------
+    cout << NULL;
+    // ---------------------
 	return 0;
+// 写的不好 对于while循环的运用还不是很熟练 足以代替if
+
+    // if (fastPointer == NULL || fastPointer -> next == NULL) {
+    //     cout << NULL;
+    // } else {
+    //     // 1. circle? fast +2 slow +1
+    //     do {
+    //         fastPointer = fastPointer -> next -> next;
+    //         slowPointer = slowPointer -> next; 
+    //     } while (fastPointer == slowPointer || fastPointer == NULL);
+
+    //     if (fastPointer == NULL)
+    //     // 2. x = z
+    //     // circle Yes: temp starts at begin;  slow starts at jiaohui     + 1 
+
+
+    //     // 3. out jiaohui head / NULL
+
+    // }
+    
+	
 }
 
--- Basic/Linked List/19.remove-nth-node-from-end-of-list.cpp
+++ Basic/Linked List/19.remove-nth-node-from-end-of-list.cpp
@@ -2,6 +2,104 @@
 // 输出：[]
 
 // 自解一：可以扫一遍确定len，然后再扫一遍删除
+// #include <iostream>
+// using namespace std;
+
+// //定义结构体
+// struct ListNode {
+// 	int val;
+// 	ListNode * next;
+// 	ListNode(int val) : val(val), next(NULL){}
+// };
+
+
+// class Solution
+// {
+// public:
+// 	/*创建单链表*/
+// 	void createList(ListNode *head)
+// 	{
+// 		int i;
+// 		ListNode* phead=head; //不破坏头指针
+// 		for(i=1;i<10;i++){
+// 			ListNode* node=new ListNode (i);
+// 			phead->next=node;
+// 			phead=node;
+// 		}
+// 		cout<<"链表创建成功!\n";
+// 	}
+	
+// 	/*打印链表*/
+// 	void printList(ListNode* head)
+// 	{
+// 		ListNode* phead=head;
+// 		while(phead!=NULL)
+// 		{
+// 			cout<<phead->val<<" ";
+// 			phead=phead->next;
+// 		}
+// 		cout<<"\n";
+// 	}		
+// };
+
+// //
+// ListNode* removeNthFromEnd(ListNode* head, int n, int len) {
+//     // len = 2 n = 1 
+//     int times = len - n;// +1 的原因是 dummyhead 开始
+//     ListNode * dummyHead = head;
+
+//     for (int i = 1; i <= times; i++){
+//         head = head -> next;
+//     }// 到达pre
+ 
+//  // remove 1 -> 2 -> 3  1 -> 3      1 -> 2  1
+//     head -> next = head -> next -> next;
+//     return dummyHead -> next;
+// }
+
+// int main()
+// {
+// 	ListNode* head=new ListNode (0);
+// 	Solution ll;
+// 	ll.createList(head);
+//     // 思路： 特殊情况：空的 ； remove的是head
+// 	// ---------------------
+
+//     // def 
+//     ListNode * dummyHead = new ListNode (-1);
+//     dummyHead -> next = head; 
+//     ListNode * temp =head;
+//     int len = 0;
+
+//     //1. len
+//     while (temp != NULL) {
+//         len ++;
+//         // update
+//         temp = temp -> next;
+//     }
+//     // cout << len << endl;
+
+//     // 2. remove
+//     if (head == NULL || head -> next == NULL) {
+//         cout << NULL << endl;
+//     } else {
+//         head = removeNthFromEnd(dummyHead, 2, len);
+//         ll.printList(head);
+//     }
+    
+// 	// ---------------------
+// 	// ll.printList(head);
+// 	return 0;
+// }
+
+
+// 输入：head = [1], n = 1
+// 输出：[]
+
+// 自解2：双指针
+// 思路：
+// fast 移动n下
+// fast到最后一个 slow就是倒数第n个
 #include <iostream>
 using namespace std;
 
@@ -42,20 +140,6 @@ public:
 	}		
 };
 
-//
-ListNode* removeNthFromEnd(ListNode* head, int n, int len) {
-    // len = 2 n = 1 
-    int times = len - n;// +1 的原因是 dummyhead 开始
-    ListNode * dummyHead = head;
-
-    for (int i = 1; i <= times; i++){
-        head = head -> next;
-    }// 到达pre
- 
- // remove 1 -> 2 -> 3  1 -> 3      1 -> 2  1
-    head -> next = head -> next -> next;
-    return dummyHead -> next;
-}
 
 int main()
 {
@@ -64,31 +148,34 @@ int main()
 	ll.createList(head);
     // 思路： 特殊情况：空的 ； remove的是head
 	// ---------------------
+	int n = 10;
+	
+	// def
+	ListNode * dummyHead = new ListNode (-1);
+	dummyHead -> next = head;
+	ListNode * fastPointer = dummyHead;
+	ListNode * slowPointer = dummyHead;
+	int i = 0;
 
-    // def 
-    ListNode * dummyHead = new ListNode (-1);
-    dummyHead -> next = head; 
-    ListNode * temp =head;
-    int len = 0;
+	if (head -> next == NULL) cout << NULL;
+	else {
+		// fast 移动n + 1下
+		for (i = 1; i <= n + 1; i++) {
+			fastPointer = fastPointer -> next;
+		}
 
-    //1. len
-    while (temp != NULL) {
-        len ++;
-        // update
-        temp = temp -> next;
-    }
-    // cout << len << endl;
+		// fast到最后一个 slow就是倒数第n个的前一个
+		while (fastPointer != NULL) {
+			fastPointer = fastPointer -> next;
+			slowPointer = slowPointer -> next;
+		} // slow是需要删除的前一个
 
-    // 2. remove
-    if (head == NULL || head -> next == NULL) {
-        cout << NULL << endl;
-    } else {
-        head = removeNthFromEnd(dummyHead, 2, len);
-        ll.printList(head);
-    }
-    
-	// ---------------------
-	// ll.printList(head);
+		// remove
+		// 1 - 2 - 3  1 - 3 slow = 1
+		slowPointer -> next = slowPointer -> next -> next;
+
+		ll.printList(dummyHead -> next);
+	} //if (head -> next == NULL)
 	return 0;
 }
 
